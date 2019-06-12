186bb3f7554c8a60ad9a27f66e080ef2f9a78ad0
Mponsubha <51688599+Mponsubha@users.noreply.github.com>
2019-06-12 12:24:36 +0530

    Create not.py

---
+++ codekata/not.py
@@ -0,0 +1,6 @@
+num=input()
+num=num.lower()
+if(num=='a' or num=='b' or num=='c' or num=='d' or num=='e' or num=='f' or num=='g' or num=='h' or num=='i' or num=='j' or num=='k' or num=='l' or num=='m' or num=='n' or num=='o' or num=='p' or num=='q' or num=='r' or num=='s' or num=='t' or num=='u' or num=='v' or num=='w' or num=='x' or num=='y' or num=='z'):
+       print("Alphabet")
+else:
+       print("No")
